f06f48fc371607832abf3e8c921205b3946b6453
VectorGits <davidoluremi0@gmail.com>
2024-08-25 18:33:30 +0100

    feat: Add Forecast component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import WeatherHeader from './components/WeatherHeader'; // Adjust the path as necessary
+import Forecast from './components/Forecast';
 
 function App() {
   return (
     <div className="App max-w-screen-lg mx-auto">
       <WeatherHeader />
+      <Forecast />
       {/* Other components can go here */}
     </div>
   );
