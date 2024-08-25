62b1d2a77661998f937587c274cc5f512910ec3a
VectorGits <davidoluremi0@gmail.com>
2024-08-25 18:56:21 +0100

    feat: Add Notification component and update App.jsx to include it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import WeatherHeader from './components/WeatherHeader'; // Adjust the path as necessary
 import Forecast from './components/Forecast';
+import Notification from './components/Notification';
 
 function App() {
   return (
     <div className="App max-w-screen-lg mx-auto">
       <WeatherHeader />
       <Forecast />
+      <Notification />
       {/* Other components can go here */}
     </div>
   );
