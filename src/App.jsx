c1645fcf4777fe6012297b39d2814b0f93005c13
VectorGits <davidoluremi0@gmail.com>
2024-08-25 15:54:46 +0100

    chore: Add Vite configuration and initial files for React app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherHeader from '../components/WeatherHeader'; // Adjust the path as necessary
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherHeader />
+      {/* Other components can go here */}
+    </div>
+  );
+}
+
+export default App;
