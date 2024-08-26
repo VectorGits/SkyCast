6f268c4bb43711f292d71d045df28d1391b9401e
VectorGits <davidoluremi0@gmail.com>
2024-08-26 12:57:14 +0100

    feat: Add Foreview component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import WeatherHeader from './components/WeatherHeader'; // Adjust the path as necessary
 import Forecast from './components/Forecast';
 import Notification from './components/Notification';
+import Foreview from './components/Foreview';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <WeatherHeader />
       <Forecast />
       <Notification />
+      <Foreview />
       {/* Other components can go here */}
     </div>
   );
