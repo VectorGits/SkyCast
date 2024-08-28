52c45b6151c6d4f2967882d4a7963ad946bff764
VectorGits <davidoluremi0@gmail.com>
2024-08-28 19:51:30 +0100

    feat: Add ForecastPage component

---
+++ src/pages/ForecastPage.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import ForecastMini from "../components/ForecastMini";
+
+const ForecastPage = () => {
+    return (
+        <div>
+            <div className="flex items-center mb-4 p-4">
+                <Link to="/home" className="flex items-center justify-center text-center font-bold px-2 py-1 mr-2 border underline rounded-lg sm:text-base md:text-xl lg:text-2xl">
+				<lord-icon
+					src="https://cdn.lordicon.com/whtfgdfm.json"
+					trigger="loop"
+					delay="2000"
+					className="sm:text-base md:text-xl lg:text-2xl"
+					style={{ transform: 'scaleX(-1)' }}>
+				</lord-icon>
+                </Link>
+                <Link to="/forecast" className="text-center text-gray-800 font-bold p-2 border rounded-lg underline sm:text-base md:text-xl lg:text-2xl">
+                    Tomorrow
+                </Link>
+            </div>
+
+			
+            <ForecastMini />
+        </div>
+    );
+};
+
+export default ForecastPage;
