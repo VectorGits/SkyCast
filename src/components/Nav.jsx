784432a0dc4efe5fdf3116dd675406abb59cc8d2
VectorGits <davidoluremi0@gmail.com>
2024-09-02 22:37:08 +0100

    refactor: Add Nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloudShowersHeavy, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
+const Nav = () => {
+  return (
+    <div className="flex justify-between items-center p-4 bg-white/30 backdrop-blur-md border border-white/20 rounded-lg m-4 sm:m-2">
+      
+    </div>
+  );
+};
+
+export default Nav;
