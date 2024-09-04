931eb7aeb3b2d919ff3c5b5fc77ec18f79e0940a
VectorGits <davidoluremi0@gmail.com>
2024-09-04 15:33:33 +0100

    refactor: Update styling and add padding to Nav container

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ import ThemeToggle from './ThemeToggle';
 
 const Nav = () => {
   return (
-    <div className="flex justify-between items-center p-4 bg-white/30 backdrop-blur-md border border-white/20 rounded-lg mx-4 sm:mx-2">
+    <div className="flex justify-between items-center px-4 py-1 bg-white/30 backdrop-blur-md border border-white/20 rounded-lg mx-4 sm:mx-2">
       <div className='bg-white/30 border border-white/20 rounded-full flex items-center p-1'>
       <lord-icon
             src="https://cdn.lordicon.com/jnikqyih.json"
