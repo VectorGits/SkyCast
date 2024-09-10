c7ef826e76437f82f9401b2760a70697a7c182a5
VectorGits <davidoluremi0@gmail.com>
2024-09-10 20:16:54 +0100

    refactor: Update to fetch and display dynamic weather data

--- src/components/WeatherHeader.jsx
+++ src/components/WeatherHeader.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import useWeatherIcon from '../hooks/useWeatherIcon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCloudShowersHeavy, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 const WeatherHeader = () => {
   const [weatherData, setWeatherData] = useState(null);
+  const [weatherLocation, setWeatherLocation] = useState({ city: '--', country: '--' });
+  const [iconCode, setIconCode] = useState('');
 
   useEffect(() => {
     const fetchWeatherData = async () => {
@@ -11,6 +14,8 @@ const WeatherHeader = () => {
         const response = await fetch('https://skycast-backend-live.onrender.com/this-weather');
         const data = await response.json();
         setWeatherData(data.weather.current);
+        setWeatherLocation(data.location);
+        setIconCode(data.weather.current.weather[0].icon); // Set the icon code from the API response
       } catch (error) {
         console.error('Error fetching weather data:', error);
       }
@@ -18,6 +23,8 @@ const WeatherHeader = () => {
 
     fetchWeatherData();
   }, []);
+
+  const weatherIcon = useWeatherIcon(iconCode); // Pass the icon code to the custom hook
 
   return (
     <div className="flex justify-between items-center p-4 bg-white/30 backdrop-blur-md border border-white/20 rounded-lg m-4 sm:m-2">
@@ -33,7 +40,7 @@ const WeatherHeader = () => {
 
       {/* Location */}
       <div className="flex items-center space-x-2">
-        <span className="sm:text-sm md:text-2xl lg:text-3xl font-medium">Lagos NG</span>
+        <span className="sm:text-sm md:text-2xl lg:text-3xl font-medium">{weatherLocation.city} {weatherLocation.country}</span>
         <FontAwesomeIcon 
           icon={faMapMarkerAlt} 
           className="sm:2xl md:text-3xl lg:text-4xl"
@@ -41,11 +48,8 @@ const WeatherHeader = () => {
       </div>
 
       {/* Weather Icon */}
-      <div>
-        <FontAwesomeIcon
-          icon={faCloudShowersHeavy}
-          className="text-sm sm:text-6xl md:text-8xl lg:text-9xl"
-        />
+      <div className="text-sm sm:text-6xl md:text-8xl lg:text-9xl">
+        {weatherIcon}
       </div>
     </div>
   );
